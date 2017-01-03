7f57cfc3fd46f40e278eca9845f760a7ce139c76
Kristine <kteich88@gmail.com>
2017-01-03 00:09:32 -0500

    Updated schema with latest migration/models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102220128) do
+ActiveRecord::Schema.define(version: 10) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "controls", force: :cascade do |t|
+    t.integer  "weapon_id"
+    t.string   "hotkey"
+    t.string   "xbox"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["weapon_id"], name: "index_controls_on_weapon_id", using: :btree
+  end
+
+  create_table "hero_stats", force: :cascade do |t|
+    t.integer  "heroes_id"
+    t.integer  "stat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["heroes_id"], name: "index_hero_stats_on_heroes_id", using: :btree
+    t.index ["stat_id"], name: "index_hero_stats_on_stat_id", using: :btree
+  end
+
   create_table "heroes", force: :cascade do |t|
     t.string   "name"
     t.string   "real_name"
-    t.string   "role"
     t.string   "age"
     t.string   "occupation"
     t.string   "base_of_operations"
@@ -35,10 +52,16 @@ ActiveRecord::Schema.define(version: 20170102220128) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "playstyles", force: :cascade do |t|
+    t.integer  "heroes_id"
+    t.string   "role"
+    t.string   "difficulty"
+    t.string   "effective_against"
+    t.string   "weak_against"
+    t.string   "synergy"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["heroes_id"], name: "index_playstyles_on_heroes_id", using: :btree
   end
 
   create_table "skins", force: :cascade do |t|
@@ -53,45 +76,46 @@ ActiveRecord::Schema.define(version: 20170102220128) do
     t.index ["heroes_id"], name: "index_skins_on_heroes_id", using: :btree
   end
 
+  create_table "specs", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stats", force: :cascade do |t|
-    t.integer  "heroes_id"
     t.integer  "health"
     t.integer  "armor"
     t.integer  "sheild"
-    t.string   "difficulty"
-    t.string   "effective_against"
-    t.string   "weak_against"
-    t.string   "synergy"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.index ["heroes_id"], name: "index_stats_on_heroes_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "weapon_skills", force: :cascade do |t|
+  create_table "weapon_specs", force: :cascade do |t|
     t.integer  "weapon_id"
-    t.integer  "skill_id"
+    t.integer  "spec_id"
     t.string   "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["skill_id"], name: "index_weapon_skills_on_skill_id", using: :btree
-    t.index ["weapon_id"], name: "index_weapon_skills_on_weapon_id", using: :btree
+    t.index ["spec_id"], name: "index_weapon_specs_on_spec_id", using: :btree
+    t.index ["weapon_id"], name: "index_weapon_specs_on_weapon_id", using: :btree
   end
 
   create_table "weapons", force: :cascade do |t|
     t.integer  "heroes_id"
     t.string   "title"
-    t.string   "hotkey"
-    t.binary   "xbox_controls"
     t.string   "description"
     t.string   "image"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["heroes_id"], name: "index_weapons_on_heroes_id", using: :btree
   end
 
+  add_foreign_key "controls", "weapons"
+  add_foreign_key "hero_stats", "heroes", column: "heroes_id"
+  add_foreign_key "hero_stats", "stats"
+  add_foreign_key "playstyles", "heroes", column: "heroes_id"
   add_foreign_key "skins", "heroes", column: "heroes_id"
-  add_foreign_key "stats", "heroes", column: "heroes_id"
-  add_foreign_key "weapon_skills", "skills"
-  add_foreign_key "weapon_skills", "weapons"
+  add_foreign_key "weapon_specs", "specs"
+  add_foreign_key "weapon_specs", "weapons"
   add_foreign_key "weapons", "heroes", column: "heroes_id"
 end
