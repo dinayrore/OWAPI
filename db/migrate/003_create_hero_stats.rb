8517ae816c06a4104e52d4b1a21e99c6b6d12277
Kristine <kteich88@gmail.com>
2017-05-19 13:37:21 -0400

    Edited migration to include stats instead of referencing a foreign key relationship.

--- db/migrate/003_create_hero_stats.rb
+++ db/migrate/003_create_hero_stats.rb
@@ -3,8 +3,10 @@ class CreateHeroStats < ActiveRecord::Migration[5.0]
   def change
     create_table :hero_stats do |t|
       t.references :heroes, index: true, foreign_key: true
-      t.references :stat, index: true, foreign_key: true
-
+      t.integer :health
+      t.integer :armor
+      t.integer :sheild
+      
       t.timestamps
     end
   end
