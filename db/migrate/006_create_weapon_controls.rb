8cd27d68845d0fe657bf835af17058a5143a2163
Kristine <kteich88@gmail.com>
2017-05-19 13:38:45 -0400

    removed index: true from control lookup.

--- db/migrate/006_create_weapon_controls.rb
+++ db/migrate/006_create_weapon_controls.rb
@@ -3,7 +3,7 @@ class CreateWeaponControls < ActiveRecord::Migration[5.0]
   def change
     create_table :weapon_controls do |t|
       t.references :weapon, index: true, foreign_key: true
-      t.references :control, index: true, foreign_key: true
+      t.references :control, foreign_key: true
 
       t.timestamps
     end
